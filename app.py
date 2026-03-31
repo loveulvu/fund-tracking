ec98fdac31c50d1f56cd2fc0c1b8ace474d4887f
loveulvu <412401474@qq.com>
2026-03-31 17:54:02 +0800

    修改端口

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ def index():
 
 if __name__ == "__main__":
     # Railway 必须动态读取 PORT 变量
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
